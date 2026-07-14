4ff12ad256692417d07e8d8eedf77eb15a8daf26
NoTinyxd <160841329+NoTinyxd@users.noreply.github.com>
2026-07-15 03:39:30 +0500

    Enhance port scanning with service names and timing
    Added service name display for open ports and improved timing output.

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 import socket
+import time
 
-host = input("Enter hostname: ") #example google.com etc
+host = input("Enter hostname: ")
 ports = [80, 443, 22, 8080] #add more ports if need
-
+open_ports=0
+start = time.perf_counter()
+ip=socket.gethostbyname(host)
+print(f"IP: {ip}")
 for port in ports:
     sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     sock.settimeout(1) 
+    services = socket.getservbyport(port)
+    #print(services)
+    
+
     scan = sock.connect_ex((host,port))
     if scan == 0:
-        print(f"{port}-> OPEN")
+        print(f"{port}({services}) -> OPEN")
+        open_ports = open_ports+1 #or just do open_ports+=1
     else:
-        print(f"{port}->{scan} ")
+        print(f"{port}({services}) -> CLOSED ")
 
-        
-sock.close()
+    sock.close()
+
+end = time.perf_counter()
+took = end-start
+if took<1:
+    print(f"\nFound {open_ports} open ports in {took*1000:.0f}ms")
+else:
+    print(f"\nFound {open_ports} open ports in {took:.1f}s")
